fbcb0bffdcc9c5cd5ded32d03db161e057fcc68c
sorpa'as plat <sorpaas@gmail.com>
2014-03-01 16:27:46 +0800

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Vagrant::Application.routes.draw do
+  devise_for :users
+  root "welcome#home"
+  get "about" => "welcome#about"
+  get "contact" => "welcome#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
